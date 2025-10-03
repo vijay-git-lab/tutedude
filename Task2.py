b524c8ef3439b2cb1b3ee7382efe0d7e9680c4a6
Vijay Kumar Rai <vijay.viju5@gmail.com>
2025-10-04 02:50:35 +0530

    concatenation of  two strings

---
+++ Task2.py
@@ -0,0 +1,10 @@
+"""
+Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+"""
+firstName = input(" Enter your first name : ")
+lastName = input(" Enter your last name : ")
+
+print(f'Hi {firstName} {lastName} ! Welcome to the Pyhton Program')
